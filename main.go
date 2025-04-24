3cba52801694a25a96c2f99f7c31899d5437b0e7
Bryn Deering <bdeering@uoguelph.ca>
2025-04-23 23:55:08 -0400

    Added simple repl

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+    "fmt"
+    "os"
+    "lemur/repl"
+)
 
+func main() {
+    fmt.Printf("Welcome to lemur alpha, we're glad you're here!\n")
+    fmt.Printf("Feel free to start by entering some commands\n")
+    repl.Start(os.Stdin, os.Stdout)
 }
---
+++ repl/repl.go
@@ -0,0 +1,30 @@
+package repl
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "strings"
+
+    "lemur/lexer"
+    "lemur/token"
+)
+
+const prompt = "> "
+
+func Start(in io.Reader, out io.Writer) {
+    scanner := bufio.NewScanner(in)
+
+    for {
+        fmt.Fprint(out, prompt)
+        if !scanner.Scan() { return }
+
+        line := strings.TrimSpace(scanner.Text())
+        if line == "q" || line == "quit" { break }
+        l := lexer.New(line)
+
+        for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+            fmt.Fprintf(out, "%+v\n", tok)
+        }
+    }
+}
