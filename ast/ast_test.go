d8446bc8f827e100ed57566a953370ed54ac34b0
Bryn Deering <bdeering@uoguelph.ca>
2025-12-29 22:09:41 -0500

    Added String() method to AST nodes + AST test for parser string output

---
+++ ast/ast_test.go
@@ -0,0 +1,29 @@
+package ast
+
+import (
+    "testing"
+
+    "lemur/token"
+)
+
+func TestString(t *testing.T) {
+    program := &Program{
+        Statements: []Statement{
+            &LetStatement{
+                Token: token.New(token.Let, "let"),
+                Name: &Identifier{
+                    Token: token.New(token.Ident, "myVar"),
+                    Value: "myVar",
+                },
+                Value: &Identifier{
+                    Token: token.New(token.Ident, "anotherVar"),
+                    Value: "anotherVar",
+                },
+            },
+        },
+    }
+
+    if program.String() != "let myVar = anotherVar;" {
+        t.Errorf("program.String() is incorrect (got %q)", program.String())
+    }
+}
